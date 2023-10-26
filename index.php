ecade77878166443e9a2954486b6af1390658cab
Serge Zozabaka <sergegabrieljr@gmail.com>
2023-10-26 16:04:38 +0200

    Variable et concat

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+// LES VARIABLES
+$chaine= "COUCOU";
+
+
+$nombre = 5;
+$tab = array(5,"test");
+
+//Affichage des variables
+echo $chaine.'<br>';
+echo $nombre;
+
+$prenom = "Serge";
+$nom = "ZOZABAKA";
+echo $prenom;
+
+echo $nom;
+
+echo '<br> <br>';
+
+
+// LES CONCATENATIONS
+$fullname = $prenom.'  '.$nom;
+echo $fullname;
+
+echo $prenom.'  '.$nom;
+?>
